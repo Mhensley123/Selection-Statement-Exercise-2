e2926f9255443b4bf90873f2b4f0887d790d2251
Maria Hensley <mscelie34@zoho.com>
2022-11-07 19:57:58 -0600

    finished selection statements 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            
+            var subject = Console.ReadLine();
+
+            switch(subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is not my cup of tea!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is very interesting but it's not my fav.");
+                    break ;
+                case "home economics":
+                    Console.WriteLine("Yes! Cooking is so much fun!");
+                    break ;
+                    default: Console.WriteLine("Sounds like a really fun class but I've never taken it before.");
+                    break;
+
+
+
+
+
+            }
         }
     }
 }
